fe0ebdb87245560d4a6aebd19709764d916bb203
kafku <kazunoko93@gmail.com>
2017-05-16 17:36:53 +0900

    specify minimum word frequency

--- nlp_utils.py
+++ nlp_utils.py
@@ -7,6 +7,7 @@ def create_word_dict(text,
                      tokenizer=word_tokenize,
                      idx_start_from=0,
                      max_vocab_size=30000,
+                     at_least=5,
                      mandatory=None,
                      signals=None):
     """
@@ -15,6 +16,7 @@ def create_word_dict(text,
         tokenizer: tokenizer applied to the text
         idx_start_from: minimum index of the word dictionary
         max_vocab_size: maximun number of vacaburary
+        at_least: minimum frequency of the words
         mandatory: mandatory words
     """
     if signals is None:
@@ -24,7 +26,7 @@ def create_word_dict(text,
     token_count = Counter(tokenizer(text))
     word_dict = defaultdict(lambda: len(word_dict) + idx_start_from)
     most_common_token = signals + mandatory
-    most_common_token.extend([x[0] for x in token_count.most_common(max_vocab_size)])
+    most_common_token.extend([x[0] for x in token_count.most_common(max_vocab_size) if x[1] >= at_least])
     vocab_size = max([word_dict[token] for token in most_common_token]) - idx_start_from + 1
     word_dict.default_factory = lambda: word_dict[signals[0]]
 
