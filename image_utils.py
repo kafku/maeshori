0d6249216b2a6434e9b3dce3a4eeb681f4f237b6
kafku <kazunoko93@gmail.com>
2017-06-02 22:50:56 +0900

    use load_img in keras

--- image_utils.py
+++ image_utils.py
@@ -2,10 +2,8 @@
 
 
 import numpy as np
-from skimage import io
-from scipy.misc import imresize
-from skimage.color import gray2rgb
 from keras.utils import np_utils
+from keras.preprocessing.image import load_img
 #from sklearn.preprocessing import OneHotEncoder
 
 def image_generator(img_list, image_size, num_classes, batch_size=200,
@@ -22,9 +20,7 @@ def image_generator(img_list, image_size, num_classes, batch_size=200,
 
             x_train = [] # raw image
             for img_path in img_list.loc[img_batch_idx]["path"]:
-                img_data = imresize(io.imread(img_path), image_size, interp='bicubic')
-                if len(img_data.shape) == 2 or img_data.shape[2] == 1: # if gray scale
-                    img_data = gray2rgb(img_data)
+                img_data = load_img(img_path, target_size=image_size)
                 x_train.append(img_data.astype(dtype))
 
             y_train = img_list.loc[img_batch_idx]["class"].values
