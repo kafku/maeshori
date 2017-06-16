0f51b7fbdbf7f9d1782a484052b5f9b863080fa8
kafku <kazunoko93@gmail.com>
2017-06-16 19:22:02 +0900

    add shuffle option

--- image_utils.py
+++ image_utils.py
@@ -6,14 +6,17 @@ from keras.utils import np_utils
 from keras.preprocessing.image import load_img, img_to_array
 #from sklearn.preprocessing import OneHotEncoder
 
-def image_generator(img_list, image_size, num_classes, batch_size=200,
-                        dtype="float32", img_process=lambda x: x):
+def image_generator(img_list, image_size, num_classes, batch_size=200, shuffle=True,
+                    dtype="float32", img_process=lambda x: x):
     """
     """
     #enc = OneHotEncoder(num_classes, dtype=np.int32, sparse=False)
 
     while True:
-        img_idx = np.random.permutation(img_list.index)
+        if shuffle:
+            img_idx = np.random.permutation(img_list.index)
+        else:
+            img_idx = img_list.index
 
         for i in range(0, len(img_list), batch_size):
             img_batch_idx = img_idx[i:min(i + batch_size, len(img_list) - 1)]
