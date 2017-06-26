4e0104eb405be4f574bbfc5f73654823b9c32ef6
kafku <kazunoko93@gmail.com>
2017-06-27 01:52:52 +0900

    save blacklist

--- image_utils.py
+++ image_utils.py
@@ -2,15 +2,44 @@
 
 
 import numpy as np
+import pandas as pd
+from scipy.misc import imread, imresize
 from keras.utils import np_utils
-from keras.preprocessing.image import load_img, img_to_array
 #from sklearn.preprocessing import OneHotEncoder
 
+
+class _Blacklist(object):
+    def __init__(self, path):
+        self.path = path
+        self.blacklist = pd.DataFrame(columns=['path'])
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, _type, value, traceback):
+        if len(self.blacklist) > 0:
+            with open(self.path, 'w') as f:
+                self.blacklist.to_csv(f, header=False)
+
+    def __del__(self):
+        if len(self.blacklist) > 0:
+            with open(self.path, 'w') as f:
+                self.blacklist.to_csv(f, header=False, index=False)
+
+    def append(self, new_path):
+        self.blacklist = self.blacklist.append(
+            pd.DataFrame({'path': np.atleast_1d(new_path)}))
+
 def image_generator(img_list, image_size, num_classes, batch_size=200, shuffle=True,
-                    dtype="float32", img_process=lambda x: x):
+                    dtype="float32", img_process=lambda x: x,
+                    blacklist='./blacklist.txt'):
     """
     """
     #enc = OneHotEncoder(num_classes, dtype=np.int32, sparse=False)
+
+    img_list = pd.DataFrame(img_list)
+    bl = _Blacklist(blacklist)
+    corrupted_image = []
 
     while True:
         if shuffle:
@@ -22,11 +51,24 @@ def image_generator(img_list, image_size, num_classes, batch_size=200, shuffle=T
             img_batch_idx = img_idx[i:min(i + batch_size, len(img_list))]
 
             x_train = [] # raw image
-            for img_path in img_list.loc[img_batch_idx]["path"]:
-                img_data = img_to_array(load_img(img_path, target_size=image_size))
-                x_train.append(img_data.astype(dtype))
+            for idx in img_batch_idx:
+                try:
+                    #img_data = img_to_array(load_img(img_list.loc[idx]['path'],
+                    #                                 target_size=image_size))
+                    img_data = imread(img_list.loc[idx]['path'], mode='RGB')
+                    img_data = imresize(img_data, image_size, interp='bicubic')
+                    x_train.append(img_data.astype(dtype))
+                except:
+                    corrupted_image.append(idx)
+                    bl.append(img_list.loc[idx]['path'])
+                    continue
 
             y_train = img_list.loc[img_batch_idx]["class"].values
 
             yield img_process(np.stack(x_train).astype(dtype)), np_utils.to_categorical(y_train, num_classes)
             #yield img_process(np.stack(x_train).astype(dtype)), enc.fit_transform(y_train.reshape(len(y_train), 1))
+
+        # remove corrupted file
+        if len(corrupted_image) > 0:
+            img_list.drop(corrupted_image, inplace=True)
+            corrupted_image = []
