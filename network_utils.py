cacf1cd17bd53bca595d9b5857810e8081a9fcc1
kafku <kazunoko93@gmail.com>
2019-04-02 18:45:18 +0900

    randomly sample edges and node-pairs

--- network_utils.py
+++ network_utils.py
@@ -3,11 +3,20 @@
 import random
 import numpy as np
 import itertools
-from more_itertools import chunked
+from more_itertools import chunked, random_product
+from .gen_utils import stack_batch_list
+
+
+def _random_node_pairs(nodes1, nodes2, n_sample):
+    """
+    return `n_sample` random node pairs (nodes1 vs node2)
+    """
+    return [random_product(nodes1, nodes2) for _ in range(n_sample)]
 
 
 def network_generator(graph, nodes=None, node_features=None, dtype='float32',
-                      batch_size=128, shuffle=True,
+                      batch_size=64, n_negative=32,
+                      random_sample=True,
                       domain1_key="domain1_input", domain2_key="domain2_input"):
     if nodes is None:
         # take it as 1-view graph if nodes is not specified
@@ -28,28 +37,39 @@ def network_generator(graph, nodes=None, node_features=None, dtype='float32',
     # TODO: enable getting edge-weight
     get_link_weight = lambda n1, n2: 1 if graph.has_edge(n1, n2) else 0
 
-    while True:
-        if shuffle:
-            nodes[0] = np.random.permutation(nodes[0])
-            nodes[1] = np.random.permutation(nodes[1])
+    def _prepare_batch(node_pairs):
+        """
+        prepare batch from node pairs
+        """
+        node1 = [x[0] for x in node_pairs]
+        node2 = [x[1] for x in node_pairs]
 
-        for node_pairs in chunked(itertools.product(*nodes), batch_size): #FIXME: make it shuffled
-            node1 = [x[0] for x in node_pairs]
-            node2 = [x[1] for x in node_pairs]
+        # view-1 feature
+        if node_features is not None and node_features[0] is not None:
+            domain1_feature = node_features[0].loc[node1].values
+        else:
+            domain1_feature = np.array([node1_idx[n] for n in node1]).reshape((len(node1), 1))
 
-            # view-1 feature
-            if node_features is not None and node_features[0] is not None:
-                domain1_feature = node_features[0].loc[node1].values
-            else:
-                domain1_feature = np.array([node1_idx[n] for n in node1]).reshape((len(node1), 1))
+        # view-2 feature
+        if node_features is not None and node_features[1] is not None:
+            domain2_feature = node_features[1].loc[node2].values
+        else:
+            domain2_feature = np.array([node2_idx[n] for n in node2]).reshape((len(node2), 1))
 
-            # view-2 feature
-            if node_features is not None and node_features[1] is not None:
-                domain2_feature = node_features[1].loc[node2].values
-            else:
-                domain2_feature = np.array([node2_idx[n] for n in node2]).reshape((len(node2), 1))
+        link_weight = np.array([get_link_weight(n1, n2) for n1, n2 in zip(node1, node2)])
+        link_weight = link_weight.reshape((len(node1), 1))
 
-            link_weight = np.array([get_link_weight(n1, n2) for n1, n2 in zip(node1, node2)])
-            link_weight.reshape((len(node1), 1))
+        return {domain1_key: domain1_feature,
+                domain2_key: domain2_feature}, link_weight
 
-            return {domain1_key: domain1_feature, domain2_key: domain2_feature}, link_weight
+    if random_sample:
+        # randomly outputs positive and negative samples
+        while True:
+            yield stack_batch_list([
+                _prepare_batch(random.sample(graph.edges, batch_size - n_negative)),
+                _prepare_batch(_random_node_pairs(nodes[0], nodes[1], n_negative))
+            ])
+    else:
+        # output all pairs
+        for node_pairs in chunked(itertools.product(*nodes), batch_size):
+            yield _prepare_batch(node_pairs)
